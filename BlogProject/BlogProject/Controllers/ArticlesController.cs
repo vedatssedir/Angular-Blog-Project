aae3840c713466041ef63fda33cc27a9f1ac3340
DESKTOP-0B7F6IE\Vedat <vedatsedir@hotmail.com>
2021-03-18 01:41:48 +0300

    first commit

---
+++ BlogProject/BlogProject/Controllers/ArticlesController.cs
@@ -0,0 +1,100 @@
+﻿using BlogProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly BlogDbContext _context;
+
+        public ArticlesController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return article;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(int id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Article>> PostArticle(Article article)
+        {
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticle", new { id = article.Id }, article);
+        }
+
+        // DELETE: api/Articles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BlogProject/BlogProject/Models/BlogDbContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BlogProject.Models
+{
+    public  class BlogDbContext : DbContext
+    {
+     
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.ToTable("Article");
+
+                entity.Property(e => e.ContentMain)
+                    .HasMaxLength(500)
+                    .HasColumnName("Content_Main");
+
+                entity.Property(e => e.ContentSummary)
+                    .HasMaxLength(500)
+                    .HasColumnName("Content_Summary");
+
+                entity.Property(e => e.Picture).HasMaxLength(500);
+
+                entity.Property(e => e.PublishDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("Publish_Date");
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Articles)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Article_Category");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.ToTable("Comment");
+
+                entity.Property(e => e.ArticleId).HasColumnName("Article_Id");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.PublishDate).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Article)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.ArticleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comment_Article");
+            });
+
+        }
+
+    }
+}
